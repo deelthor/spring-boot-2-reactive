8a3e1573dd78bc7e19d9cc9649d0a5a464ac7a59
deelthor <thorsten.deelmann@codecentric.de>
2017-10-05 10:57:09 +0200

    service und client

---
+++ whiskybar-client/src/main/java/de/deelthor/whiskybar/client/WhiskyBarClientApplication.java
@@ -0,0 +1,12 @@
+package de.deelthor.whiskybar.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiskyBarClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhiskyBarClientApplication.class, args);
+    }
+}
