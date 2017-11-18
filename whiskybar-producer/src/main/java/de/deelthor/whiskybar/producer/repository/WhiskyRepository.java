c528ec9af68cf7aa555daef9539271f0dd9ba099
deelthor <thorsten.deelmann@codecentric.de>
2017-11-18 13:38:29 +0100

    use ReactiveCassandraRepository

--- whiskybar-producer/src/main/java/de/deelthor/whiskybar/producer/repository/WhiskyRepository.java
+++ whiskybar-producer/src/main/java/de/deelthor/whiskybar/producer/repository/WhiskyRepository.java
@@ -2,13 +2,14 @@ package de.deelthor.whiskybar.producer.repository;
 
 import de.deelthor.whiskybar.producer.entity.Whisky;
 import org.springframework.data.cassandra.repository.Query;
+import org.springframework.data.cassandra.repository.ReactiveCassandraRepository;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
-public interface WhiskyRepository extends ReactiveCrudRepository<Whisky, UUID> {
+public interface WhiskyRepository extends ReactiveCassandraRepository<Whisky, UUID> {
 
     @Query("SELECT * FROM whiskies WHERE id = ?0 ALLOW FILTERING")
     Mono<Whisky> findById(UUID id);
